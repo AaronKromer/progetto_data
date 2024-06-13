d79f0f7381c25d285b494b5b66f5828cd58dd185
Lorenzo Giampietro <lorenzo.giampietro68@gmail.com>
2024-06-13 17:35:03 +0200

    education income plot

--- correlation.py
+++ correlation.py
@@ -5,25 +5,25 @@ import matplotlib.pyplot as plt
 original_df = pd.read_csv('initial_dataset.csv')
 anonymized_df = pd.read_csv('generalized_dataset.csv')
 
-ordered_ages = ["30-20" ,"40-30", "50-40","60-50","70-60"]
-anonymized_df['age'] = pd.Categorical(anonymized_df['age'], categories=ordered_ages, ordered=True)
+# ordered_ages = ["30-20" ,"40-30", "50-40","60-50","70-60"]
+# anonymized_df['role'] = pd.Categorical(anonymized_df['role'], categories=ordered_ages, ordered=True)
 
 
 
 plt.figure(figsize=(14, 6))
 
 plt.subplot(1, 2, 1)
-sns.boxplot(x='age', y='salary', data=original_df)
-plt.title('Initial distribution of salary by age')
-plt.xlabel('Age')
+sns.boxplot(x='education', y='salary', data=original_df)
+plt.title('Initial distribution of salary by education')
+plt.xlabel('Education')
 plt.ylabel('Salary')
 plt.xticks(rotation=45)
 plt.grid(True)
 
 plt.subplot(1, 2, 2)
-sns.boxplot(x='age', y='salary', data=anonymized_df)
-plt.title('Generalized distribution of salary by age')
-plt.xlabel('Age')
+sns.boxplot(x='education', y='salary', data=anonymized_df)
+plt.title('Generalized distribution of salary by education')
+plt.xlabel('Education')
 plt.ylabel('Salary')
 plt.xticks(rotation=45)
 plt.grid(True)
